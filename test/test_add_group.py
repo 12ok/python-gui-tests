0a5bd73a0bf2a43f7ed89d3f010cee1222df1453
Olga Krasavina <krasavina.olga.12.12@gmail.com>
2020-07-31 21:48:05 +0300

    Add test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,4 +1,3 @@
-
 def test_add_group(app):
     old_list = app.groups.get_group_list()
     app.groups.add_new_group("my group")
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,8 +5,8 @@ def test_delete_group(app):
     if len(app.groups.get_group_list()) == 0:
         app.groups.add_new_group("group")
     old_groups = app.groups.get_group_list()
-    print(old_groups)
-    app.groups.delete_group(randrange(len(old_groups)))
+    index = randrange(len(old_groups))
+    app.groups.delete_group(index)
     new_groups = app.groups.get_group_list()
-    old_groups.remove(old_groups[0])
+    old_groups.remove(old_groups[index])
     assert sorted(old_groups) == sorted(new_groups)
