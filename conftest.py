81965024c6fcc6ee0ec41551bd92082d3aa401c6
Olga Krasavina <krasavina.olga.12.12@gmail.com>
2020-08-01 00:13:51 +0300

    Add read from exel

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+import openpyxl
+import os
+
 import pytest
 
 from fixture.application import Application
@@ -8,3 +11,19 @@ def app(request):
     fixture = Application("C:\\Users\\HP\\Desktop\\addressbook\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("xlsx_"):
+            testdata = load_from_xlsx(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_xlsx(file):
+    wb = openpyxl.load_workbook(os.path.join(os.path.dirname(os.path.realpath(__file__)), "%s.xlsx" % file))
+    sheet = wb.worksheets[0]
+    excel_list = []
+    for row in sheet.rows:
+        excel_list.append(row[0].value)
+    return excel_list
